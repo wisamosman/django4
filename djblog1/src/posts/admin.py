f06fa7ce1a419a7631035c73da4344199299104f
wisamosman <viesam_1987@hotmail.com>
2023-05-30 00:52:48 +0200

    add author

--- djblog1/src/posts/admin.py
+++ djblog1/src/posts/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import post , author
-admin.site.register(post)
+from .models import post , author 
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ['title','author','publish_date']
+    list_filter = ['title','author','publish_date']
+    search_fields = ['title','content']
+
+
+
+
+admin.site.register(post,postAdmin)
 admin.site.register(author)
+
+
+
+
 
 # Register your models here.
