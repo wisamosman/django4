0a4391e2b8da9f0c34f8991ef83b678392b58836
wisamosman <viesam_1987@hotmail.com>
2023-05-30 01:10:36 +0200

    add author

--- djblog1/src/posts/views.py
+++ djblog1/src/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import post
+
+def post_list(request):
+    data = post.objects.all()
+    return render(request, 'posts/posts.html', {})
+
 
 # Create your views here.
--- djblog1/src/project1/urls.py
+++ djblog1/src/project1/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', post_list),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
