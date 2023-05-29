6c176e1c93be0872977db5617a1e953762fd5db3
wisamosman <viesam_1987@hotmail.com>
2023-05-30 01:39:23 +0200

    add author

---
+++ djblog1/src/posts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import post
+
+class postForm(forms.ModelForm):
+    class Meta:
+        model = post
+        fields = '__all__'
--- djblog1/src/posts/views.py
+++ djblog1/src/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import post
+from .forms import postForm
 
 def post_list(request):
     data = post.objects.all()
@@ -8,4 +9,10 @@ def post_list(request):
 def post_detail(request,post_id):
     data = post.objects.get(id=post_id)
     return render(request,'posts/detail.html',{'post':data})
+
+
+
+def new_post(request):
+    form = postForm()
+    return render(request,'posts/new.html', {'form':form})
 # Create your views here.
