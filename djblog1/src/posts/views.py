86fcc1734c4baa620f9f654ac7521893d93d6c43
wisamosman <viesam_1987@hotmail.com>
2023-05-30 01:21:26 +0200

    add author

--- djblog1/src/posts/views.py
+++ djblog1/src/posts/views.py
@@ -5,5 +5,7 @@ def post_list(request):
     data = post.objects.all()
     return render(request, 'posts/posts.html', {'posts':data})
 
-
+def post_detail(request,post_id):
+    data = post.objects.get(id=post_id)
+    return render(request,'posts/detail.html',{'post':data})
 # Create your views here.
