2022ffe5543d0a6cc26205f3b08f0ef0633a76c5
wisamosman <viesam_1987@hotmail.com>
2023-05-29 23:49:33 +0200

    add author

--- djblog1/src/posts/models.py
+++ djblog1/src/posts/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class post(models.Model):
-    title = models.CharField(max_length=100)
-    publish_date = models.DateTimeField()
-    content = models.TextField(max_length=15000)
+
 
 
 
 class author(models.Model):
     name = models.CharField(max_length=100)
-    bio = models.TextField(max_length=500)    
+    bio = models.TextField(max_length=500) 
+
+
+
+
+
+
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    publish_date = models.DateTimeField()
+    content = models.TextField(max_length=15000)
+    author = models.ForeignKey(author,related_name='post_author',on_delete=models.CASCADE)
+
+
+
+   
