b04f218823e86264708cad9b5c733b33b002f8bf
wisamosman <viesam_1987@hotmail.com>
2023-05-29 23:36:18 +0200

    add author

--- djblog1/src/posts/models.py
+++ djblog1/src/posts/models.py
@@ -5,3 +5,9 @@ class post(models.Model):
     title = models.CharField(max_length=100)
     publish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
+
+
+
+class author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)    
